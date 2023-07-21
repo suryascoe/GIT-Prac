ae874b0112ab5fbaafc0edaade773dca0009a436
Surya Singh <surya.scoe3154@gmail.com>
2023-07-21 18:41:03 +0530

    Test

--- run.py
+++ run.py
@@ -2,8 +2,4 @@ def mainApp():
     return "main features on master branch"
 
 def secondaryFeature():
-<<<<<<< Updated upstream
-    return "this is secondary feature."
-=======
     return "this is......."
->>>>>>> Stashed changes
