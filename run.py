f43a06a39f5a03679ba6bfcbfba4de51cf3eb1d8
Surya Singh <surya.scoe3154@gmail.com>
2023-07-21 17:59:37 +0530

    Learning GIT Stash

--- run.py
+++ run.py
@@ -1,19 +1,2 @@
-
-
-def isValidUserName(username):
-    if username == "hello":
-        return True
-    
-    return False
-
-def isValidPassword(password):
-    if password == "1234":
-        return True
-    
-    return False
-
-if isValidUserName("hello"):
-    if isValidPassword("1234"):
-        print("Test Success")
-
-# After Added Calling Function.
+def mainApp():
+    return "main features on master branch"
