db8b37c971dc8a05e34582b8290836b20dbc8db8
Surya Singh <surya.scoe3154@gmail.com>
2023-07-21 18:36:01 +0530

    Again Secondary Feature

--- run.py
+++ run.py
@@ -2,4 +2,8 @@ def mainApp():
     return "main features on master branch"
 
 def secondaryFeature():
+<<<<<<< Updated upstream
     return "this is secondary feature."
+=======
+    return "this is......."
+>>>>>>> Stashed changes
